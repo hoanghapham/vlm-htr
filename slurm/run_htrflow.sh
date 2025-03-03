e214033d253553470cd0cde9faee260b29b91f22
hoanghapham <hoangha.20021992@gmail.com>
2025-03-03 16:11:06 +0100

    add slurm script

---
+++ slurm/run_htrflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J htrflow
+#SBATCH -o logs_uppmax/htrflow.out
+#SBATCH -e logs_uppmax/htrflow.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+
+INPUT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models/data/poliskammare"
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models"
+
+cd $PROJECT_DIR
+
+python workpad/run_htrflow.py --input_dir $INPUT_DIR
