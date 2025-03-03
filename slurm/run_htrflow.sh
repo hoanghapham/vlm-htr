b4ef7987161471b1fbd94b00b130be577b638123
hoanghapham <hoangha.20021992@gmail.com>
2025-03-03 16:51:17 +0100

    increase run time for slurm script, update script to evaluate HTRFlow

--- slurm/run_htrflow.sh
+++ slurm/run_htrflow.sh
@@ -4,7 +4,7 @@
 #SBATCH -M snowy
 #SBATCH -p node
 #SBATCH -N 1
-#SBATCH -t 24:00:00
+#SBATCH -t 48:00:00
 #SBATCH -J htrflow
 #SBATCH -o logs_uppmax/htrflow.out
 #SBATCH -e logs_uppmax/htrflow.err
--- workpad/evaluate_htrflow.py
+++ workpad/evaluate_htrflow.py
@@ -1,15 +1,35 @@
-gt_pages_dir = "/Users/hoanghapham/Projects/visual-language-models/data/sample_image_page"
-candidate_pages_dir = "/Users/hoanghapham/Projects/visual-language-models/workpad/outputs/page/Göteborgs_poliskammare_före_1900__A_II__1a__1868_"
-
+#%%
 from htrflow.evaluate import CER, WER, BagOfWords, read_xmls
+from pathlib import Path
+import sys
+import pandas as pd
 
-candidates = read_xmls(candidate_pages_dir)
-ground_truth = read_xmls(gt_pages_dir)
+PROJECT_DIR = Path(__file__).parent.parent
+sys.path.append(str(PROJECT_DIR))
 
-page = list(ground_truth.keys())[0]
+from src.utils.file_tools import list_files
 
-wer_metric = WER()
+gt_dir = PROJECT_DIR / "data/poliskammare/page_xmls"
+candidate_dir = PROJECT_DIR / "workpad/outputs/page"
 
-value = wer_metric(ground_truth[page], candidates[page])
+# Read PageXML
+ground_truth = read_xmls(gt_dir)
+candidates = read_xmls(candidate_dir)
 
+pages = list(ground_truth.keys())
+cer_metrics = []
+wer_metrics = []
+bow_metrics = []
+
+metrics = [CER(), WER(), BagOfWords()]
+
+dfs = []
+pages = [page for page in ground_truth if page in candidates and ground_truth[page].num_words > 0]
+
+for metric in metrics:
+    values = [metric(ground_truth[page], candidates[page]) for page in pages]
+    df = pd.DataFrame(values, index=pages)
+    dfs.append(df)
+
+df = pd.concat(dfs, axis=1)
 # %%
