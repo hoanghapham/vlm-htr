54118c8ddadd0c27b92cf1b4c18af65dd21d6a9a
hoanghapham <hoangha.20021992@gmail.com>
2025-03-09 13:29:21 +0100

    update script to train htr_line model

--- workpad/finetune_florence_htr_line.py
+++ workpad/finetune_florence_htr_line.py
@@ -1,18 +1,30 @@
-
+#%%
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from transformers import AutoModelForCausalLM, AutoProcessor, get_scheduler
-from datasets import load_dataset
+from datasets import load_from_disk, concatenate_datasets
 from tqdm import tqdm
-import numpy as np
 
+import json
+import typing
+
+import sys
 from pathlib import Path
 PROJECT_DIR = Path(__file__).parent.parent
+sys.path.append(str(PROJECT_DIR))
+
+from src.logger import CustomLogger
+from src.utils import gen_split_indices
+
+
+#%%
+
+logger = CustomLogger("ft_florence_htr_line")
 
 # Load model
-print("Load model")
+logger.info("Load model")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_path = "microsoft/Florence-2-base-ft"
 
@@ -32,18 +44,15 @@ for param in model.vision_tower.parameters():
   param.is_trainable = True
 
 
-# Create VQA Dataset
+#%%
 # Load data
-print("Load data")
-data = load_dataset("Riksarkivet/gota_hovratt_seg", trust_remote_code=True, name="text_recognition")
+logger.info("Load data")
+DATA_DIR = PROJECT_DIR / "data/poliskammare_line"
 
-total_samples = len(data["train"])
-np.random.seed(42)
-train_idx = np.random.choice(range(total_samples), size=int(0.8 * total_samples), replace=False)
-val_idx = np.array([i for i in range(total_samples) if i not in train_idx])
+# Collect page lists
+page_list = [str(path) for path in DATA_DIR.glob("*") if path.is_dir()]
 
-assert len(train_idx) + len(val_idx) == total_samples, "Mismatch subset sizes after splitting"
-
+# Create dataset
 class HTRDataset(Dataset):
 
     def __init__(self, data):
@@ -54,10 +63,23 @@ class HTRDataset(Dataset):
 
     def __getitem__(self, idx):
         example = self.data[idx]
-        question = "<HTR>Print out the text in this image"
+        question = "<SwedishHTR>Print out the text in this image"
         answer = example["transcription"]
         image = example['image'].convert("RGB")
         return question, answer, image
+    
+    def select(self, indices: typing.Iterable):
+        subset = [self.data[int(idx)] for idx in indices]
+        return HTRDataset(subset)
+
+
+def create_dataset(path_list: list[str | Path]) -> HTRDataset:
+    dsets = []
+    for path in path_list:
+        dsets.append(load_from_disk(path))
+    data = concatenate_datasets(dsets)
+
+    return HTRDataset(data)
 
 
 # Create train loader & validate loader
@@ -65,14 +87,34 @@ class HTRDataset(Dataset):
 def collate_fn(batch):
     questions, answers, images = zip(*batch)
     inputs = processor(text=list(questions), images=list(images), return_tensors="pt", padding=True).to(device)
-    return inputs, answers
-
+    labels = processor.tokenizer(text=answers, return_tensors="pt", padding=True, return_token_type_ids=False).input_ids.to(device)
+    return dict(
+        input_ids=inputs["input_ids"], 
+        pixel_values=inputs["pixel_values"], 
+        labels=labels
+    )
 
 
 # Subset train & validate set
-train_dataset = HTRDataset(data['train'].select(train_idx))
-val_dataset = HTRDataset(data['train'].select(val_idx))
 
+train_indices, val_indices, test_indices = gen_split_indices(len(page_list), seed=42)
+split_info = {
+    "train": [page_list[idx] for idx in train_indices],
+    "validation": [page_list[idx] for idx in val_indices],
+    "test": [page_list[idx] for idx in test_indices]
+}
+
+with open(DATA_DIR / "split_info.json", "w") as f:
+    json.dump(split_info, f)
+
+
+train_pages = [page_list[idx] for idx in train_indices]
+val_pages = [page_list[idx] for idx in val_indices]
+
+train_dataset = create_dataset(train_pages)
+val_dataset = create_dataset(val_pages)
+
+# Create data loader
 batch_size = 2
 num_workers = 0
 
@@ -82,30 +124,37 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size,
                           collate_fn=collate_fn, num_workers=num_workers)
 
 
+#%%
 # Train
-epochs = 5
+epochs = 2
 optimizer = AdamW(model.parameters(), lr=1e-6)
 num_training_steps = epochs * len(train_loader)
-
-
 lr_scheduler = get_scheduler(name="linear", optimizer=optimizer,
                               num_warmup_steps=0, num_training_steps=num_training_steps,)
 
+break_idx = int(0.5 * len(train_loader))
 
+MODEL_DIR = PROJECT_DIR / "models/florence-2-base-ft-htr-line"
+
+if not MODEL_DIR.exists():
+    MODEL_DIR.mkdir(parents=True)
+
+#%%
 for epoch in range(epochs):
     model.train()
     train_loss = 0
 
     # Inputs is the processed tuple (text, image)
-    for inputs, answers in tqdm(train_loader, desc=f"Training Epoch {epoch + 1}/{epochs}"):
+    iterator = tqdm(train_loader, desc=f"Training Epoch {epoch + 1}/{epochs}")
 
-        # Get input (text tokens and )
-        input_ids = inputs["input_ids"]
-        pixel_values = inputs["pixel_values"]
-        labels = processor.tokenizer(text=answers, return_tensors="pt", padding=True, return_token_type_ids=False).input_ids.to(device)
+    for batch_idx, batch_data in enumerate(iterator):
+        
+        # Skip half of the batches
+        if batch_idx > break_idx:
+            break
 
         # Predict output
-        outputs = model(input_ids=input_ids, pixel_values=pixel_values, labels=labels)
+        outputs = model(**batch_data)
 
         # Calculate loss, then backward
         loss = outputs.loss
@@ -119,32 +168,31 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         train_loss += loss.item()
 
+        iterator.set_postfix({"loss": loss.item()})
+    
+    # Save checkpoint
+    model_info = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }
+    torch.save(model_info, MODEL_DIR / f"checkpoint_epoch_{epoch:03d}.pt")
+
     avg_train_loss = train_loss / len(train_loader)
-    print(f"Average Training Loss: {avg_train_loss}")
+    logger.info(f"Average Training Loss: {avg_train_loss}")
 
 
+#%%
 # Check validation loss
 model.eval()
 val_loss = 0
 with torch.no_grad():
     for batch in tqdm(val_loader, desc=f"Validation Epoch {epoch + 1}/{epochs}"):
-        inputs, answers = batch
-        input_ids = inputs["input_ids"]
-        pixel_values = inputs["pixel_values"]
-        labels = processor.tokenizer(text=answers, return_tensors="pt", padding=True, return_token_type_ids=False).input_ids.to(device)
-        outputs = model(input_ids=input_ids, pixel_values=pixel_values, labels=labels)
+        outputs = model(**batch)
         loss = outputs.loss
         val_loss += loss.item()
 
-print("Average Validation Loss: ", val_loss / len(val_loader))
 
-
-
-# Save model
-print("Save model")
-model_out_dir = PROJECT_DIR / "models/florence-2-base-ft-hovratt-htr-line"
-
-if not model_out_dir.exists():
-    model_out_dir.mkdir(parents=True)
-
-model.save_pretrained(model_out_dir, from_pt=True)
+avg_val_loss = val_loss / len(val_loader)
+logger.info(f"Average Validation Loss: , {avg_val_loss}")
