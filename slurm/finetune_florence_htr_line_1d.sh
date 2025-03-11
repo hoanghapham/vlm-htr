722572dcffaf3aab8a68e80ccd79a6000ffff955
hoanghapham <hoangha.20021992@gmail.com>
2025-03-11 14:31:01 +0100

    add scripts to run for 1 day

---
+++ slurm/finetune_florence_htr_line_1d.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 2-00:00
+#SBATCH -J 1d_finetune_florence_htr_line
+#SBATCH -o logs_uppmax/1d_finetune_florence_htr_line.out
+#SBATCH -e logs_uppmax/1d_finetune_florence_htr_line.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models"
+
+cd $PROJECT_DIR
+
+python workpad/finetune_florence_htr_line.py
---
+++ slurm/finetune_florence_htr_region_1d.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 1-00:00
+#SBATCH -J 1d_finetune_florence_htr_region
+#SBATCH -o logs_uppmax/1d_finetune_florence_htr_region.out
+#SBATCH -e logs_uppmax/1d_finetune_florence_htr_region.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models"
+
+cd $PROJECT_DIR
+
+python workpad/finetune_florence_htr_region.py --epochs 10 --use-batch-pct 1 --batch-size 2
