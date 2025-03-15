7a02c2f12ca6e250ae6e424b7267716eafc39176
hoanghapham <hoangha.20021992@gmail.com>
2025-03-15 16:59:22 +0100

    add script to evalute on hovratt_region dataset

---
+++ slurm/eval__florence_htr_region__hovratt_region.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J eval__florence2_htr_region__hovratt_region
+#SBATCH -o logs_uppmax/eval__florence2_htr_region__hovratt_region.out
+#SBATCH -e logs_uppmax/eval__florence2_htr_region__hovratt_region.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models"
+cd $PROJECT_DIR
+
+python workpad/evaluate_florence_htr.py \
+    --model-name florence-2-base-ft-htr-region \
+    --input-dir $PROJECT_DIR/data/hovratt_region
