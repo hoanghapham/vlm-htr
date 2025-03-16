715df5bb4ffb8536762921e580826ac92e40be1b
hoanghapham <hoangha.20021992@gmail.com>
2025-03-16 10:35:53 +0100

    add script to convert checkpoints

---
+++ slurm/convert_checkpoints.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 2-00:00
+#SBATCH -J convert_checkpoints
+#SBATCH -o logs_uppmax/convert_checkpoints.out
+#SBATCH -e logs_uppmax/convert_checkpoints.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/visual-language-models"
+
+cd $PROJECT_DIR
+
+python workpad/convert_checkpoints.py --model-dir models/florence-2-base-ft-htr-line
