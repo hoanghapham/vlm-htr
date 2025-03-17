db7b4772b02c30f4bf65a446dfea6bdd90be315a
hoanghapham <hoangha.20021992@gmail.com>
2025-03-17 14:31:41 +0100

    disable evaluation

--- workpad/finetune_gpt2.py
+++ workpad/finetune_gpt2.py
@@ -31,8 +31,6 @@ torch.cuda.empty_cache()
 
 from datasets import load_dataset
 
-from huggingface_hub import login
-
 train_subset_stream = load_dataset("oscar-corpus/OSCAR-2201",
                         # use_auth_token=True, # required
                         language="sv", 
@@ -80,22 +78,22 @@ random.seed(seed)
 # Training arguments
 training_args = TrainingArguments(
     output_dir='./checkpoints',
-    evaluation_strategy='steps',
-    eval_steps=100,  # Evaluate after every 1000 steps. 
+    # evaluation_strategy='steps',
+    # eval_steps=100,  # Evaluate after every 1000 steps. 
     save_strategy='steps',
-    save_steps=100, # must be a round multiple of the evaluation steps for early stopping to work??
+    save_steps=50, # must be a round multiple of the evaluation steps for early stopping to work??
     num_train_epochs=3,
     load_best_model_at_end=True, # Necessary for early stopping
     per_device_train_batch_size=BATCH_SIZE,
-    per_device_eval_batch_size=BATCH_SIZE,
+    # per_device_eval_batch_size=BATCH_SIZE,
     logging_dir='./logs',
-    logging_steps=100,
+    logging_steps=50,
     seed=seed,
     learning_rate=2e-5, # default is 5e-5? 
     weight_decay=0.01,
     gradient_accumulation_steps=2,
-    metric_for_best_model="eval_loss",  # Track evaluation loss
-    max_steps=5
+    # metric_for_best_model="eval_loss",  # Track evaluation loss
+    max_steps=200
     #fp16=False,
     #bf16=True,
 )
@@ -112,10 +110,10 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=train_subset,
-    eval_dataset=eval_subset,
+    # eval_dataset=eval_subset,
     data_collator=data_collator,
-    callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],  # Stop if no improvement after 3 evaluations
-    compute_metrics=compute_metrics,
+    # callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],  # Stop if no improvement after 3 evaluations
+    # compute_metrics=compute_metrics,
 )
 
 torch.cuda.empty_cache()
