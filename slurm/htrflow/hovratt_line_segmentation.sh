fd6183d97393efb9365337a673e8407344c25b13
hoanghapham <hoangha.20021992@gmail.com>
2025-03-20 14:05:02 +0100

    add scripts to run htr on hovratt

---
+++ slurm/htrflow/hovratt_line_segmentation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 2-00:00
+#SBATCH -J htrflow/hovratt_line_segmentation
+#SBATCH -o logs_uppmax/htrflow/hovratt_line_segmentation.out
+#SBATCH -e logs_uppmax/htrflow/hovratt_line_segmentation.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+
+INPUT_DIR="/proj/uppmax2024-2-24/hapham/vlm/data/hovratt"
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python workpad/run_htrflow.py \
+    --input_dir $INPUT_DIR \
+    --split-info-fp $PROJECT_DIR/data/hovratt_line/split_info.json \
+    --config_path $PROJECT_DIR/configs/htrflow/hovratt_line_segmentation.yaml
---
+++ slurm/htrflow/hovratt_text_recognition.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 2-00:00
+#SBATCH -J htrflow/hovratt_text_recognition
+#SBATCH -o logs_uppmax/htrflow/hovratt_text_recognition.out
+#SBATCH -e logs_uppmax/htrflow/hovratt_text_recognition.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+
+INPUT_DIR="/proj/uppmax2024-2-24/hapham/vlm/data/hovratt"
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python workpad/run_htrflow_pipeline.py \
+    --config_path $PROJECT_DIR/configs/htrflow/hovratt_text_recognition.yaml \
+    --input_dir $INPUT_DIR \
+    --split-info-fp $PROJECT_DIR/data/hovratt_line/split_info.json \
