93661738acb353124dc5db3268f42ec778a41e55
hoanghapham <hoangha.20021992@gmail.com>
2025-03-22 11:45:07 +0100

    fix

--- pipelines/data_process/create_htr_split_info.py
+++ pipelines/data_process/create_htr_split_info.py
@@ -5,7 +5,7 @@ PROJECT_DIR = Path(__file__).parent.parent.parent
 sys.path.append(str(PROJECT_DIR))
 
 from argparse import ArgumentParser
-from src.data_process.utils import create_split_info
+from src.data_process.utils import create_htr_split_info
 from src.file_tools import read_json_file
 
 #%%
@@ -19,7 +19,7 @@ DATA_DIR = Path(args.data_dir)
 
 split_info_path = DATA_DIR / "split_info.json"
 if not split_info_path.exists() or args.regen == "true":
-    create_split_info(DATA_DIR, seed=42, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1)
+    create_htr_split_info(DATA_DIR, seed=42, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1)
 
 
 #%%
