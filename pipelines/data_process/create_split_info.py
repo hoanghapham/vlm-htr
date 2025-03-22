de79586acf7d7faf3a3c7c34888cb962b0820c8a
hoanghapham <hoangha.20021992@gmail.com>
2025-03-22 11:14:19 +0100

    update script

--- pipelines/data_process/create_split_info.py
+++ pipelines/data_process/create_split_info.py
@@ -12,17 +12,19 @@ from src.file_tools import read_json_file
 
 parser = ArgumentParser()
 parser.add_argument("--data-dir", required=True)
+parser.add_argument("--regen", default="false")
 args = parser.parse_args()
 
 DATA_DIR = Path(args.data_dir)
 
 split_info_path = DATA_DIR / "split_info.json"
-create_split_info(DATA_DIR, seed=42, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1)
+if not split_info_path.exists() or args.regen == "true":
+    create_split_info(DATA_DIR, seed=42, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1)
 
 
 #%%
 
-split_info = read_json_file("/Users/hoanghapham/Projects/thesis-data/riksarkivet/split_info.json")
+split_info = read_json_file(split_info_path)
 
-print(len(split_info["train"]), len(split_info["validation"]), len(split_info["test"]))
+print("Train, val, test size:", len(split_info["train"]), len(split_info["validation"]), len(split_info["test"]))
 # %%
