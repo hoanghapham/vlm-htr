cf1445d29cbeb736b2e8e760f1adadc00512b79a
hoanghapham <hoangha.20021992@gmail.com>
2025-03-23 12:38:15 +0100

    add script to evaluate trocr finetuned

---
+++ slurm/eval/trocr_base/ft_htr_line/polis_line.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J eval/trocr_base/ft_htr_line/polis_line
+#SBATCH -o logs_uppmax/eval/trocr_base/ft_htr_line/polis_line.out
+#SBATCH -e logs_uppmax/eval/trocr_base/ft_htr_line/polis_line.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+cd $PROJECT_DIR
+
+python pipelines/evaluate/evaluate_trocr_htr.py \
+    --model-name trocr_base__ft_htr_line \
+    --input-dir $PROJECT_DIR/data/polis_line \
+    --use-split-info true
