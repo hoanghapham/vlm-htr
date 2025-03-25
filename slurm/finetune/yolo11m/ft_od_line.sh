2c325c357fda61e8c45a954e9f25caccaf16af0f
hoanghapham <hoangha.20021992@gmail.com>
2025-03-25 17:31:38 +0100

    add script to finetune yolo - line detection

--- slurm/finetune/yolo11m/ft_od_line.sh
+++ slurm/finetune/yolo11m/ft_od_line.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 3-00:00
-#SBATCH -J finetune/yolo11m/ft_od_region
-#SBATCH -o logs_uppmax/finetune/yolo11m/ft_od_region.out
-#SBATCH -e logs_uppmax/finetune/yolo11m/ft_od_region.err
+#SBATCH -J finetune/yolo11m/ft_od_line
+#SBATCH -o logs_uppmax/finetune/yolo11m/ft_od_line.out
+#SBATCH -e logs_uppmax/finetune/yolo11m/ft_od_line.err
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -15,7 +15,7 @@
 
 source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
 PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
-DATA_DIR="$PROJECT_DIR/data/yolo/region_detection"
+DATA_DIR="$PROJECT_DIR/data/yolo/line_detection"
 
 cd $PROJECT_DIR
 
