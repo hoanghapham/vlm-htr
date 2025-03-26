78319ed89f3e29de38ee132e045802d5cf5c76bf
hoanghapham <hoangha.20021992@gmail.com>
2025-03-26 21:53:17 +0100

    update slurm script

--- slurm/finetune/trocr_base/mixed__line__ocr.sh
+++ slurm/finetune/trocr_base/mixed__line__ocr.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 2-00:00
-#SBATCH -J finetune/trocr_base/ft_htr_line__riksarkivet
-#SBATCH -o logs_uppmax/finetune/trocr_base/ft_htr_line__riksarkivet.out
-#SBATCH -e logs_uppmax/finetune/trocr_base/ft_htr_line__riksarkivet.err
+#SBATCH -J finetune/trocr_base/mixed__line__ocr
+#SBATCH -o logs_uppmax/finetune/trocr_base/mixed__line__ocr.out
+#SBATCH -e logs_uppmax/finetune/trocr_base/mixed__line__ocr.err
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -15,14 +15,13 @@
 
 source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
 PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
-DATA_DIR=$PROJECT_DIR/data/riksarkivet
 
 cd $PROJECT_DIR
 
 python pipelines/train/finetune_trocr_htr.py \
-    --data-dir $DATA_DIR \
-    --model-name trocr_base__ft_htr_line__riksarkivet \
-    --num-train-epochs 5 \
-    --max-train-steps 10000 \
+    --data-dir $PROJECT_DIR/data/variants/mixed/line/ \
+    --model-name trocr_base__mixed__line__ocr \
+    --num-train-epochs 4 \
+    --max-train-steps 140000 \
     --batch-size 10 \
-    --logging-interval 1000
+    --logging-interval 10000
