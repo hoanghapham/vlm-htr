8a3d4fde5ab2887516bb5eeeaa8968ea1738a89e
hoanghapham <hoangha.20021992@gmail.com>
2025-03-26 20:42:23 +0100

    update script to create cropped lines

--- pipelines/data_process/create_line_dataset.py
+++ pipelines/data_process/create_line_dataset.py
@@ -13,12 +13,12 @@ from src.logger import CustomLogger
 
 # Argparse
 parser = ArgumentParser()
-parser.add_argument("--input-dir", required=True)
+parser.add_argument("--input-path", required=True)
 parser.add_argument("--output-dir", required=True)
 args = parser.parse_args()
 
 # # Setup
-INPUT_DIR = Path(args.input_dir)
+INPUT_PATH = Path(args.INPUT_PATH)
 OUTPUT_DIR = Path(args.output_dir)
 
 if not OUTPUT_DIR.exists():
@@ -29,20 +29,26 @@ logger = CustomLogger("create_line_dataset")
 
 #%%
 builder = RunningTextDatasetBuilder()
-
 img_ext = [".tif", ".jpg", ".jpeg", ".png"]
 
-img_paths = [str(path) for path in sorted(Path.glob(INPUT_DIR / "images", pattern="**/*")) if path.suffix in img_ext]
-xml_paths = [str(path) for path in sorted(Path.glob(INPUT_DIR / "page_xmls", pattern="**/*.xml"))]
+img_paths = []
+xml_paths = []
+
+if INPUT_PATH.is_dir():
+    img_paths = [str(path) for path in sorted(Path.glob(INPUT_PATH / "images", pattern="**/*")) if path.suffix in img_ext]
+    xml_paths = [str(path) for path in sorted(Path.glob(INPUT_PATH / "page_xmls", pattern="**/*.xml"))]
+
+elif INPUT_PATH.is_file():
+    img_paths = [INPUT_PATH]
+    xml_paths = [Path(str(INPUT_PATH).replace("images", "page_xmls")).with_suffix(".xml")]
 
 # assert len(img_path) == len(all_xml_paths) > 0, \
 #     f"Invalid length, or mismatch: {len(all_img_paths)} - {len(all_xml_paths)}"
 
 imgs_xmls = list(zip(
-        sorted(img_paths, key=lambda x: Path(x).stem), 
-        sorted(xml_paths, key=lambda x: Path(x).stem)
-    )
-)
+    sorted(img_paths, key=lambda x: Path(x).stem), 
+    sorted(xml_paths, key=lambda x: Path(x).stem)
+))
 
 ttl_samples = len(img_paths)
 
