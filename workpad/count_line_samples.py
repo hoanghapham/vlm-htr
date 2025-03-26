1b9e1d5a7ac0c9139483fe557879f00dd70091f5
hoanghapham <hoangha.20021992@gmail.com>
2025-03-26 20:20:43 +0100

    fix

--- workpad/count_line_samples.py
+++ workpad/count_line_samples.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-PROJECT_DIR = Path(__file__).parent.parent.parent
+PROJECT_DIR = Path(__file__).parent.parent
 sys.path.append(str(PROJECT_DIR))
 
 from argparse import ArgumentParser
