7c5fce9fb41e528cc02c743ccbdef44c9a40d836
hoanghapham <hoangha.20021992@gmail.com>
2025-03-27 15:05:53 +0100

    use lora

--- slurm/finetune/florence_base/mixed__page__reg_od_demo.sh
+++ slurm/finetune/florence_base/mixed__page__reg_od_demo.sh
@@ -25,5 +25,5 @@ python pipelines/train/finetune_florence_od.py \
     --max-train-steps 2000 \
     --batch-size 2 \
     --logging-interval 500 \
-    --use-lora false \
+    --use-lora true \
     --detect-class region
