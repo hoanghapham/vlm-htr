0a7589244060d92c4f95071187edf21197680ce6
hoanghapham <hoangha.20021992@gmail.com>
2025-03-28 22:35:27 +0100

    fix

--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -30,7 +30,7 @@ class Ratio:
     __radd__ = __add__  # redirects int + Ratio to __add__
 
     def __float__(self):
-        return 0 if self.b == 0 else float(self.a / self.b)
+        return 0.0 if self.b == 0 else float(self.a / self.b)
 
     def __gt__(self, other):
         return float(self) > float(other)
