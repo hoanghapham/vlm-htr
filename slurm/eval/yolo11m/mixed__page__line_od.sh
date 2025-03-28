05ce5dddb4663434b5f8af71bac7e26d1efe72a8
hoanghapham <hoangha.20021992@gmail.com>
2025-03-28 16:41:00 +0100

    add script to eval yolo11m

---
+++ slurm/eval/yolo11m/mixed__page__line_od.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 12:00:00
+#SBATCH -J eval/yolo11m/mixed__page__line_od
+#SBATCH -o logs_uppmax/eval/yolo11m/mixed__page__line_od.out
+#SBATCH -e logs_uppmax/eval/yolo11m/mixed__page__line_od.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+cd $PROJECT_DIR
+
+python pipelines/evaluate/eval_yolo_od.py \
+    --input-dir $PROJECT_DIR/variants/mixed/page \
+    --model-name yolo11m__mixed__page__line_od \
+    --object-class line
