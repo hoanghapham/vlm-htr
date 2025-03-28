2fad660f784b53a2c1b58d982db60c7f0b684d82
hoanghapham <hoangha.20021992@gmail.com>
2025-03-28 18:36:05 +0100

    add slurm script to create region dataset

--- slurm/data_process/create_region_dataset.sh
+++ slurm/data_process/create_region_dataset.sh
@@ -6,9 +6,9 @@
 #SBATCH -N 1
 #SBATCH -t 24:00:00
 #SBATCH -C mem512GB
-#SBATCH -J create_hovratt_region_dataset
-#SBATCH -o logs_uppmax/create_hovratt_region_dataset.out
-#SBATCH -e logs_uppmax/create_hovratt_region_dataset.err
+#SBATCH -J create_region_dataset
+#SBATCH -o logs_uppmax/data_process/create_region_dataset.out
+#SBATCH -e logs_uppmax/data_process/create_region_dataset.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
 
@@ -16,11 +16,7 @@
 source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
 PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
 DATA_DIR="/proj/uppmax2024-2-24/hapham/data/hovratt"
-OUTPUT_DIR="$PROJECT_DIR/data/hovratt_region"
 
 cd $PROJECT_DIR
 
-python workpad/create_region_dataset.py \
-    --data-dir $DATA_DIR \
-    --output-dir $OUTPUT_DIR \
-    --img-extension .jpg
+python pipelines/data_process/create_region_dataset.py 
