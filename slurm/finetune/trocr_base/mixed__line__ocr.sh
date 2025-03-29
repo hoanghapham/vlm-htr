6c573efdd471a1532d8c47ddadc6e5b0174c7ab4
hoanghapham <hoangha.20021992@gmail.com>
2025-03-29 14:27:11 +0100

    fix

--- slurm/finetune/trocr_base/mixed__line__ocr.sh
+++ slurm/finetune/trocr_base/mixed__line__ocr.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 3-00:00
 #SBATCH -J finetune/trocr_base/mixed__line__ocr
 #SBATCH -o logs_uppmax/finetune/trocr_base/mixed__line__ocr.out
-#SBATCH -e logs_uppmax/finetune/trocr_base/mixed__line__ocr.err
+#SBATCH -e logs_uppmax/finetune/trocr_base/mixed__line__ocr.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,7 +19,7 @@ PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
 cd $PROJECT_DIR
 
 python pipelines/train/finetune_trocr_ocr.py \
-    --data-dir $PROJECT_DIR/data/data/lines/mixed/ \
+    --data-dir $PROJECT_DIR/data/lines/mixed/ \
     --model-name trocr_base__mixed__line__ocr \
     --num-train-epochs 4 \
     --max-train-steps 140000 \
