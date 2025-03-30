5cfb36c4fcff7fa7e7ed57db79fd15524f19659a
hoanghapham <hoangha.20021992@gmail.com>
2025-03-30 11:54:14 +0200

    add script to train yolo line seg

---
+++ slurm/finetune/yolo11m/mixed__region__line_seg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 3-00:00
+#SBATCH -J finetune/yolo11m/mixed__region__line_seg
+#SBATCH -o logs_uppmax/finetune/yolo11m/mixed__region__line_seg.out
+#SBATCH -e logs_uppmax/finetune/yolo11m/mixed__region__line_seg.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+# Should train for 20 epochs
+python pipelines/train/finetune_yolo.py \
+    --data-dir $PROJECT_DIR/data/yolo/mixed/inst_seg_lines_within_regions \
+    --data-fraction 1 \
+    --base-model-path $PROJECT_DIR/models/yolo_base/yolo11m-seg.pt \
+    --model-name yolo11m_seg__mixed__region__line_seg \
+    --epochs 20 \
+    --batch-size 6 \
+    --img-size 1280 \
