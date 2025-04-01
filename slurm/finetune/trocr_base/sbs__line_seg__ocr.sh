922b52ab440196e7c458686acf9c3b7741c4702f
hoanghapham <hoangha.20021992@gmail.com>
2025-04-01 22:29:43 +0200

    aupdate script for trocr

--- slurm/finetune/trocr_base/sbs__line_seg__ocr.sh
+++ slurm/finetune/trocr_base/sbs__line_seg__ocr.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 3-00:00
-#SBATCH -J finetune/trocr_base/mixed__line_seg__ocr
-#SBATCH -o logs_uppmax/finetune/trocr_base/mixed__line_seg__ocr.out
-#SBATCH -e logs_uppmax/finetune/trocr_base/mixed__line_seg__ocr.out
+#SBATCH -J finetune/trocr_base/sbs__line_seg__ocr
+#SBATCH -o logs_uppmax/finetune/trocr_base/sbs__line_seg__ocr.out
+#SBATCH -e logs_uppmax/finetune/trocr_base/sbs__line_seg__ocr.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,8 +19,8 @@ PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
 cd $PROJECT_DIR
 
 python pipelines/train/finetune_trocr_ocr.py \
-    --data-dir $PROJECT_DIR/data/line_seg/mixed/ \
-    --model-name trocr_base__mixed__line_seg__ocr \
+    --data-dir $PROJECT_DIR/data/line_seg/sbs/ \
+    --model-name trocr_base__sbs__line_seg__ocr \
     --num-train-epochs 4 \
     --max-train-steps 150000 \
     --batch-size 6 \
