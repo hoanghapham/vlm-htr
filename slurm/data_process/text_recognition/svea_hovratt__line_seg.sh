4d37c3c0d1dfbe83b5094ce5df05afd125081bdb
hoanghapham <hoangha.20021992@gmail.com>
2025-04-01 21:59:26 +0200

    add scripts to create line_seg

---
+++ slurm/data_process/text_recognition/svea_hovratt__line_seg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -C mem512GB
+#SBATCH -J data_process/create_svea_hovratt__line_seg
+#SBATCH -o logs_uppmax/data_process/create_svea_hovratt__line_seg.out
+#SBATCH -e logs_uppmax/data_process/create_svea_hovratt__line_seg.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python pipelines/data_process/create_arrow_datasets.py \
+    --raw-data-dir $PROJECT_DIR/data/raw/riksarkivet/svea_hovratt/ \
+    --dataset-type text_recognition__line_seg \
+    --output-data-dir $PROJECT_DIR/data/processed/riksarkivet/text_recognition__line_seg
