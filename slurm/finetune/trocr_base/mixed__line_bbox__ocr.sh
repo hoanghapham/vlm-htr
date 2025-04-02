2e59156257c9f6b2105a042f786411ce35ba44d4
hoanghapham <hoangha.20021992@gmail.com>
2025-04-02 20:53:30 +0200

    add scripts to train on line_bbox dataset

---
+++ slurm/finetune/trocr_base/mixed__line_bbox__ocr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 3-00:00
+#SBATCH -J finetune/trocr_base/mixed__line_bbox__ocr
+#SBATCH -o logs_uppmax/finetune/trocr_base/mixed__line_bbox__ocr.out
+#SBATCH -e logs_uppmax/finetune/trocr_base/mixed__line_bbox__ocr.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
+
+cd $PROJECT_DIR
+
+python pipelines/train/finetune_trocr_ocr.py \
+    --data-dir $PROJECT_DIR/data/line_bbox/mixed/ \
+    --model-name trocr_base__mixed__line_bbox__ocr \
+    --num-train-epochs 4 \
+    --max-train-steps 140000 \
+    --batch-size 6 \
+    --logging-interval 10000
