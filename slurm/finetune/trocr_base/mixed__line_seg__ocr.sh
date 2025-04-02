5bcac81cacab532f04223a7be2debbe2189995d2
hoanghapham <hoangha.20021992@gmail.com>
2025-04-02 22:31:20 +0200

    reduce batch size

--- slurm/finetune/trocr_base/mixed__line_seg__ocr.sh
+++ slurm/finetune/trocr_base/mixed__line_seg__ocr.sh
@@ -22,6 +22,6 @@ python pipelines/train/finetune_trocr_ocr.py \
     --data-dir $PROJECT_DIR/data/line_seg/mixed/ \
     --model-name trocr_base__mixed__line_seg__ocr \
     --num-train-epochs 4 \
-    --max-train-steps 140000 \
-    --batch-size 6 \
+    --max-train-steps 200000 \
+    --batch-size 4 \
     --logging-interval 10000
