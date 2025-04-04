c921fded409d0fc8f7698297e837869d87e6401b
hoanghapham <hoangha.20021992@gmail.com>
2025-04-04 10:52:00 +0200

    change model name

--- slurm/finetune/florence_base/sbs__line_cropped__line_seg.sh
+++ slurm/finetune/florence_base/sbs__line_cropped__line_seg.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 3-00:00
-#SBATCH -J finetune/florence_base/mixed__page__single_line_seg
-#SBATCH -o logs_uppmax/finetune/florence_base/mixed__page__single_line_seg.out
-#SBATCH -e logs_uppmax/finetune/florence_base/mixed__page__single_line_seg.out
+#SBATCH -J finetune/florence_base/sbs__line_cropped__line_seg
+#SBATCH -o logs_uppmax/finetune/florence_base/sbs__line_cropped__line_seg.out
+#SBATCH -e logs_uppmax/finetune/florence_base/sbs__line_cropped__line_seg.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,8 +19,8 @@ PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
 cd $PROJECT_DIR
 
 python pipelines/train/finetune_florence_single_line_seg.py \
-    --data-dir $PROJECT_DIR/data/page/mixed/ \
-    --model-name florence_base__mixed__page__single_line_seg \
+    --data-dir $PROJECT_DIR/data/page/sbs/ \
+    --model-name florence_base__sbs__line_cropped__line_seg \
     --num-train-epochs 2 \
     --max-train-steps 220000 \
     --batch-size 2 \
