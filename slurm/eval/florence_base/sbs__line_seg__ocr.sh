35da22e81e860982403b5f378de568b677a801d5
hoanghapham <hoangha.20021992@gmail.com>
2025-04-08 20:56:17 +0200

    add script to evaluate florence line_seg ocr

---
+++ slurm/eval/florence_base/sbs__line_seg__ocr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 3-00:00
+#SBATCH -J eval/florence_base/sbs__line_seg__ocr
+#SBATCH -o logs_uppmax/eval/florence_base/sbs__line_seg__ocr.out
+#SBATCH -e logs_uppmax/eval/florence_base/sbs__line_seg__ocr.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python pipelines/eval/eval_florence_ocr.py \
+    --model-name florence_base__sbs__line_seg__ocr \
+    --data-dir $PROJECT_DIR/data/lines/sbs/test/ \
+    --checkpoint best
