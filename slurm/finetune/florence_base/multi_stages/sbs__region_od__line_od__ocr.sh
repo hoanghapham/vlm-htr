e6bd543932e4acaf7e75582735c229dd39bd54ab
hoanghapham <hoangha.20021992@gmail.com>
2025-04-08 07:53:55 +0200

    fix split info

--- slurm/finetune/florence_base/multi_stages/sbs__region_od__line_od__ocr.sh
+++ slurm/finetune/florence_base/multi_stages/sbs__region_od__line_od__ocr.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 3-00:00
-#SBATCH -J finetune/florence_base/mixed__region_od__line_od__ocr
-#SBATCH -o logs_uppmax/finetune/florence_base/mixed__region_od__line_od__ocr.out
-#SBATCH -e logs_uppmax/finetune/florence_base/mixed__region_od__line_od__ocr.out
+#SBATCH -J finetune/florence_base/sbs__region_od__line_od__ocr
+#SBATCH -o logs_uppmax/finetune/florence_base/sbs__region_od__line_od__ocr.out
+#SBATCH -e logs_uppmax/finetune/florence_base/sbs__region_od__line_od__ocr.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,8 +19,8 @@ PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
 cd $PROJECT_DIR
 
 python pipelines/train/finetune_florence_ocr.py \
-    --data-dir $PROJECT_DIR/data/line_bbox/mixed/ \
-    --model-name florence_base__mixed__region_od__line_od__ocr \
+    --data-dir $PROJECT_DIR/data/line_bbox/sbs/ \
+    --model-name florence_base__sbs__region_od__line_od__ocr \
     --num-train-epochs 2 \
     --max-train-steps 220000 \
     --batch-size 2 \
