09acee6b3207eefda9c9072bb43f4c88cc3f6a33
hoanghapham <hoangha.20021992@gmail.com>
2025-04-30 12:40:01 +0200

    fix wrong model name

--- slurm/eval/florence_base/sbs__page__region_od__vanilla.sh
+++ slurm/eval/florence_base/sbs__page__region_od__vanilla.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 1-00:00
-#SBATCH -J eval/florence_base/sbs__page__line_od__vanilla
-#SBATCH -o logs_uppmax/eval/florence_base/sbs__page__line_od__vanilla.out
-#SBATCH -e logs_uppmax/eval/florence_base/sbs__page__line_od__vanilla.out
+#SBATCH -J eval/florence_base/sbs__page__region_od__vanilla
+#SBATCH -o logs_uppmax/eval/florence_base/sbs__page__region_od__vanilla.out
+#SBATCH -e logs_uppmax/eval/florence_base/sbs__page__region_od__vanilla.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,7 +19,7 @@ PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
 cd $PROJECT_DIR
 
 python pipelines/eval/eval_florence_od.py \
-    --model-name florence_base__sbs__page__line_od \
+    --model-name florence_base__sbs__page__region_od \
     --data-dir $PROJECT_DIR/data/page/sbs/test \
     --checkpoint vanilla \
     --object-class line
