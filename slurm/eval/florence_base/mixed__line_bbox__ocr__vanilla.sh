1bed0287c3d322096e431da1fb2934ce87627bba
hoanghapham <hoangha.20021992@gmail.com>
2025-04-30 15:39:59 +0200

    add scripts to evaluate vanilla models on ocr

---
+++ slurm/eval/florence_base/mixed__line_bbox__ocr__vanilla.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2024-2-24
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 3-00:00
+#SBATCH -J eval/florence_base/mixed__line_bbox__ocr__vanilla
+#SBATCH -o logs_uppmax/eval/florence_base/mixed__line_bbox__ocr__vanilla.out
+#SBATCH -e logs_uppmax/eval/florence_base/mixed__line_bbox__ocr__vanilla.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python pipelines/eval/eval_florence_ocr.py \
+    --model-name florence_base__mixed__line_bbox__ocr__vanilla \
+    --data-dir $PROJECT_DIR/data/line_bbox/mixed/test/ \
+    --checkpoint vanilla
---
+++ slurm/eval/trocr_base/sbs__line_bbox__ocr__vanilla.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2024-2-24
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 1-00:00
+#SBATCH -J eval/trocr_base/sbs__line_bbox__ocr__vanilla
+#SBATCH -o logs_uppmax/eval/trocr_base/sbs__line_bbox__ocr__vanilla.out
+#SBATCH -e logs_uppmax/eval/trocr_base/sbs__line_bbox__ocr__vanilla.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR="/proj/uppmax2024-2-24/hapham/vlm"
+
+cd $PROJECT_DIR
+
+python pipelines/eval/eval_trocr_ocr.py \
+    --model-name trocr_base__sbs__line_bbox__ocr__vanilla \
+    --data-dir $PROJECT_DIR/data/line_bbox/sbs/test/ \
+    --checkpoint vanilla \
+    --batch-size 10
