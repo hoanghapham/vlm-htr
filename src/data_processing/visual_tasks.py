e463e8f91aede9c2916aeb0321cde760370f1b2f
hoanghapham <hoangha.20021992@gmail.com>
2025-05-03 08:43:50 +0200

    add dataset for region line od

--- src/data_processing/visual_tasks.py
+++ src/data_processing/visual_tasks.py
@@ -345,7 +345,7 @@ class BaseImgXMLDataset(ABC):
                 self.xml_paths.append(xml)
     
     
-class TextRegionBboxDataset(BaseImgXMLDataset):
+class PageRegionODDataset(BaseImgXMLDataset):
 
     def __init__(self, data_dir: str | Path):
         super().__init__(data_dir=data_dir)
@@ -366,8 +366,54 @@ class TextRegionBboxDataset(BaseImgXMLDataset):
             xml_path=self.xml_paths[idx]
         )
 
-        
-class TextLineBboxDataset(BaseImgXMLDataset):
+
+class RegionLineODDataset(BaseImgXMLDataset):
+
+    def __init__(self, data_dir: str | Path):
+        super().__init__(data_dir=data_dir)
+
+        self.idx_to_img_path: list[Path] = []
+        self.idx_to_xml_path: list[Path] = []
+        self.region_bboxes = []
+        self.region_polygons = []
+        self.region_lines = []
+
+        # Preload region - line data
+        for idx, (img, xml) in enumerate(zip(self.img_paths, self.xml_paths)):
+            regions = self.xmlparser.get_regions(xml)
+            
+            for region in regions:
+                if len(region["lines"]) > 0:
+                    self.idx_to_img_path.append(img)
+                    self.idx_to_xml_path.append(xml)
+                    self.region_bboxes.append(region["bbox"])
+                    self.region_polygons.append(region["polygon"])
+                    self.region_lines.append(region["lines"])
+
+
+    def __getitem__(self, idx):
+        """Return one region image & line bboxes"""
+        img_filename    = self.idx_to_img_path[idx].stem
+        img_volume      = self.idx_to_img_path[idx].parent.name
+        full_image      = PILImage.open(self.idx_to_img_path[idx]).convert("RGB")
+
+        region_polygon = self.region_polygons[idx]
+        region_image = crop_image(full_image, region_polygon)
+
+        region_lines = self.region_lines[idx]
+        line_bboxes = [data["bbox"] for data in region_lines]
+
+        return dict(
+            image=region_image,
+            bboxes=line_bboxes,
+            img_volume=img_volume,
+            img_filename=img_filename,
+            img_path=self.idx_to_img_path[idx],
+            xml_path=self.idx_to_img_path[idx]
+        )
+
+
+class PageLineODDataset(BaseImgXMLDataset):
 
     def __init__(self, data_dir: str | Path):
         super().__init__(data_dir=data_dir)
