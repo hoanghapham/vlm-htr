40ec2b85bdf738ad28d69f03cca3201130747611
hoanghapham <hoangha.20021992@gmail.com>
2025-05-05 22:29:01 +0200

    add script to evaluate florence region od - line od - ocr

---
+++ slurm/eval/pipeline/mixed__florence__line_od__line_od__ocr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2020-2-2
+#SBATCH -M snowy
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J eval/pipeline/mixed__florence__region_od__line_od__ocr
+#SBATCH -o logs_uppmax/eval/pipeline/mixed__florence__region_od__line_od__ocr.out
+#SBATCH -e logs_uppmax/eval/pipeline/mixed__florence__region_od__line_od__ocr.out
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
+
+
+source activate /crex/proj/uppmax2024-2-24/hapham/envs/vlm
+PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
+cd $PROJECT_DIR
+
+python pipelines/eval/eval_pipeline_florence__region_od__line_od__ocr.py \
+    --split-type mixed \
+    --batch-size 2
