7aad8b58e80e23ad5faea8f0d12a50c8c6395860
hoanghapham <hoangha.20021992@gmail.com>
2025-05-05 07:22:59 +0200

    update script to evaluate line od - ocr - line od

--- slurm/eval/florence_base/mixed__line_od__ocr__line_od.sh
+++ slurm/eval/florence_base/mixed__line_od__ocr__line_od.sh
@@ -5,9 +5,9 @@
 #SBATCH -p node
 #SBATCH -N 1
 #SBATCH -t 3-00:00
-#SBATCH -J eval/florence_base/sbs__line_od__line_od
-#SBATCH -o logs_uppmax/eval/florence_base/sbs__line_od__line_od.out
-#SBATCH -e logs_uppmax/eval/florence_base/sbs__line_od__line_od.out
+#SBATCH -J eval/florence_base/mixed__line_od__ocr__line_od
+#SBATCH -o logs_uppmax/eval/florence_base/mixed__line_od__ocr__line_od.out
+#SBATCH -e logs_uppmax/eval/florence_base/mixed__line_od__ocr__line_od.out
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hoang-ha.pham.1833@student.uu.se
@@ -19,9 +19,9 @@ PROJECT_DIR=/proj/uppmax2024-2-24/hapham/vlm
 cd $PROJECT_DIR
 
 python pipelines/eval/eval_florence_od.py \
-    --model-name florence_base__sbs__line_od__ocr \
-    --data-dir $PROJECT_DIR/data/page/sbs/test/ \
-    --output-dir $PROJECT_DIR/evaluations/florence_base__sbs__line_od__line_od/ \
+    --model-name florence_base__mixed__line_od__ocr \
+    --data-dir $PROJECT_DIR/data/page/mixed/test/ \
+    --output-dir $PROJECT_DIR/evaluations/florence_base__mixed__line_od__ocr__line_od/ \
     --object-class line \
     --checkpoint best \
     --batch-size 4
