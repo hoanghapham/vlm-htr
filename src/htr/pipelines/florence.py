80077d86445f96b26b54e9c3c1e2bf86c3e44c4d
hoanghapham <hoangha.20021992@gmail.com>
2025-05-15 16:09:59 +0200

    minor updates

--- src/htr/pipelines/florence.py
+++ src/htr/pipelines/florence.py
@@ -17,7 +17,6 @@ from src.data_processing.florence import predict, FlorenceTask
 from src.data_processing.visual_tasks import bbox_xyxy_to_polygon, polygon_to_bbox_xyxy, crop_image, get_cover_bbox
 from src.data_types import Page, Region, Line, ODOutput
 from src.htr.utils import (
-    sort_consider_margins,
     sort_page,
     correct_line_bbox_coords,
     correct_line_polygon_coords,
@@ -97,7 +96,7 @@ class LineDetection(Step):
     def preprocess(self):
         pass
 
-    def postprocess(self, image: PILImage, detected_objs: ODOutput) -> PILImage:
+    def postprocess(self, image: PILImage, detected_objs: ODOutput) -> list[PILImage]:
         cropped_imgs = []
         for polygon in detected_objs.polygons:
             cropped_imgs.append(crop_image(image, polygon))
@@ -217,7 +216,7 @@ class FlorencePipeline():
         line_od_model_path: str | Path = None, 
         line_seg_model_path: str | Path = None, 
         ocr_model_path: str | Path = None, 
-        batch_size: str = 2,
+        batch_size: int = 2,
         device: str = "cuda",
         logger: CustomLogger = None,
     ):
