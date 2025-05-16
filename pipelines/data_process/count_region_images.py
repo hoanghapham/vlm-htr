c83c32055585d8da3d7ba47fcb602636d2e3dd68
hoanghapham <hoangha.20021992@gmail.com>
2025-05-16 09:21:57 +0200

    count region images

---
+++ pipelines/data_process/count_region_images.py
@@ -0,0 +1,29 @@
+#%%
+import sys
+from pathlib import Path
+PROJECT_DIR = Path(__file__).parent.parent.parent
+sys.path.append(str(PROJECT_DIR))
+
+from argparse import ArgumentParser
+
+from src.data_processing.florence import FlorenceRegionLineODDataset
+from src.logger import CustomLogger
+
+parser = ArgumentParser()
+parser.add_argument("--data-dir", required=True)
+args = parser.parse_args()
+
+DATA_DIR = Path(args.data_dir)
+
+train_dataset   = FlorenceRegionLineODDataset(DATA_DIR / "train")
+val_dataset     = FlorenceRegionLineODDataset(DATA_DIR / "val")
+test_dataset    = FlorenceRegionLineODDataset(DATA_DIR / "test")
+
+logger = CustomLogger(f"count_region_images", log_to_local=True)
+logger.info(f"Number of train images: {len(train_dataset)}")
+logger.info(f"Number of val images: {len(val_dataset)}")
+logger.info(f"Number of test images: {len(test_dataset)}")
+
+logger.info(f"Valid train images: {len(train_dataset.xml_paths)}")
+logger.info(f"Valid val images: {len(val_dataset.xml_paths)}")
+logger.info(f"Valid test images: {len(test_dataset.xml_paths)}")
